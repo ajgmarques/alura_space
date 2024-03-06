072fe7a57c1e3ff456cd1d5da8598fa85eb54a25
António Marques <janacorreio@gmail.com>
2024-03-05 21:49:23 -0300

    Login logout e cadastro com messages

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -25,3 +25,52 @@ class LoginForms(forms.Form):
         ),
     )
 
+
+class CadastroForms(forms.Form):
+    nome_usuario = forms.CharField(
+        label='Nome completo',
+        required=True,
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Ex: António Marques'
+            }
+        )
+    )
+
+    email_usuario = forms.EmailField(
+        label='Email',
+        required=True,
+        max_length=100,
+        widget=forms.EmailInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Ex: enderecomail@maildominio.com'
+            }
+        )
+    )
+
+    senha_usuario = forms.CharField(
+        label='Senha',
+        required=True,
+        max_length=70,
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Digite a sua senha',
+            }
+        )
+    )
+
+    senha_usuario_confirma = forms.CharField(
+        label='Confirme a sua senha',
+        required=True,
+        max_length=70,
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Digite novamente a sua senha',
+            }
+        )
+    )
